a3f49d8529ab5566982c314213363a01fd82d36b
JGZimek <jgzimek@gmail.com>
2024-10-24 09:12:05 +0200

    Refactor scheduling module and add buzzer, PIR, and MQTT tasks

--- src/scheduling/scheduling.cpp
+++ src/scheduling/scheduling.cpp
@@ -5,6 +5,9 @@
 TaskHandle_t wifiTaskHandle = NULL;
 TaskHandle_t rfidTaskHandle = NULL;
 TaskHandle_t pinpadTaskHandle = NULL;
+TaskHandle_t buzzerTaskHandle = NULL;
+TaskHandle_t pirTaskHandle = NULL;
+TaskHandle_t mqttTaskHandle = NULL;
 
 bool esp_setup()
 {
@@ -23,6 +26,24 @@ bool esp_setup()
     if (!init_pinpad())
     {
         ESP_LOGE(SCHEDULING_TAG, "Failed to initialize Pinpad");
+        return false;
+    }
+
+    if (!init_buzzer())
+    {
+        ESP_LOGE(SCHEDULING_TAG, "Failed to initialize Buzzer");
+        return false;
+    }
+
+    if (!init_pir())
+    {
+        ESP_LOGE(SCHEDULING_TAG, "Failed to initialize PIR");
+        return false;
+    }
+
+    if (!init_mqtt())
+    {
+        ESP_LOGE(SCHEDULING_TAG, "Failed to initialize MQTT");
         return false;
     }
 
@@ -59,6 +80,33 @@ void pinpadTask(void *pvParameters)
     {
         handle_pinpad();
         vTaskDelay(PINPAD_READ_FREQ / portTICK_PERIOD_MS);
+    }
+}
+
+void buzzerTask(void *pvParameters)
+{
+    while (1)
+    {
+        handle_buzzer();
+        vTaskDelay(BUZZER_READ_FREQ / portTICK_PERIOD_MS);
+    }
+}
+
+void pirTask(void *pvParameters)
+{
+    while (1)
+    {
+        handle_pir();
+        vTaskDelay(PIR_READ_FREQ / portTICK_PERIOD_MS);
+    }
+}
+
+void mqttTask(void *pvParameters)
+{
+    while (1)
+    {
+        handle_mqtt();
+        vTaskDelay(MQTT_READ_FREQ / portTICK_PERIOD_MS);
     }
 }
 
@@ -110,6 +158,51 @@ bool init_scheduling()
     if (result != pdPASS)
     {
         ESP_LOGE(SCHEDULING_TAG, "Failed to create Pinpad Task");
+        return false;
+    }
+
+    result = xTaskCreatePinnedToCore(
+        buzzerTask,
+        "Buzzer Task",
+        BUZZER_TASK_STACK_SIZE,
+        NULL,
+        BUZZER_TASK_PRIORITY,
+        &buzzerTaskHandle,
+        BUZZER_CORE);
+
+    if (result != pdPASS)
+    {
+        ESP_LOGE(SCHEDULING_TAG, "Failed to create Buzzer Task");
+        return false;
+    }
+
+    result = xTaskCreatePinnedToCore(
+        pirTask,
+        "PIR Task",
+        PIR_TASK_STACK_SIZE,
+        NULL,
+        PIR_TASK_PRIORITY,
+        &pirTaskHandle,
+        PIR_CORE);
+
+    if (result != pdPASS)
+    {
+        ESP_LOGE(SCHEDULING_TAG, "Failed to create PIR Task");
+        return false;
+    }
+
+    result = xTaskCreatePinnedToCore(
+        mqttTask,
+        "MQTT Task",
+        MQTT_TASK_STACK_SIZE,
+        NULL,
+        MQTT_TASK_PRIORITY,
+        &mqttTaskHandle,
+        MQTT_CORE);
+
+    if (result != pdPASS)
+    {
+        ESP_LOGE(SCHEDULING_TAG, "Failed to create MQTT Task");
         return false;
     }
 
