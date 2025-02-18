6f3ce3c41ec90f3fb4b39201ea9a0d2fcd4db76a
mostekpollaka <bkuznia1234@gmail.com>
2025-02-18 17:45:14 +0100

    Env_measurement works

--- environment_management/src/env_measurement/env_measurement.hpp
+++ environment_management/src/env_measurement/env_measurement.hpp
@@ -2,7 +2,10 @@
 
 #include <Arduino.h>
 #include "esp_log.h"
+#include <Adafruit_BME280.h>
 
+// Inicjalizacja czujnika BME280
 bool init_env_measurement();
 
+// Funkcja do obsługi pomiarów środowiskowych
 void handle_env_measurement();
