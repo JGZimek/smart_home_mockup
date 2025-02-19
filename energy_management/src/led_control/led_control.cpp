eaa8400fdd9d928b76d17b1be59e822d56979627
mostekpollaka <bkuznia1234@gmail.com>
2025-02-19 13:44:39 +0100

    The RGB LED works

--- energy_management/src/led_control/led_control.cpp
+++ energy_management/src/led_control/led_control.cpp
@@ -1,12 +1,39 @@
 #include "led_control.hpp"
 
+#define LED_CONTROL_TAG "led_control"
+
+// LED Array
+CRGB led[NUM_LEDS];
+
+/*
+ * @brief Initializes the LED system.
+ */
 bool init_led_control()
 {
-    ESP_LOGI("led_control", "Initializing LED control...");
+    ESP_LOGI(LED_CONTROL_TAG, "Initializing LED control...");
+    FastLED.addLeds<WS2812, LED_PIN, RGB>(led, NUM_LEDS).setCorrection(TypicalLEDStrip);
+    FastLED.setBrightness(100); // Set LED brightness
     return true;
 }
 
+/*
+ * @brief Sets the LED to a specific color.
+ */
+void set_led_color(uint8_t red, uint8_t green, uint8_t blue)
+{
+    led[0] = CRGB(red, green, blue);
+    FastLED.show();
+    ESP_LOGI(LED_CONTROL_TAG, "LED Color -> R: %d, G: %d, B: %d", red, green, blue);
+}
+
+/*
+ * @brief Handles LED color.
+ */
 void handle_led_control()
 {
-    ESP_LOGI("led_control", "Handling LED control...");
+    int red=0;
+    int green=0;
+    int blue=255;
+
+    set_led_color(red, green, blue);
 }
--- energy_management/src/led_control/led_control.hpp
+++ energy_management/src/led_control/led_control.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
 #include <Arduino.h>
+#include <FastLED.h>
 #include "esp_log.h"
+
+// LED Configuration
+#define LED_PIN 14
+#define NUM_LEDS 1
 
 /*
  * @brief Initializes the LED control.
@@ -9,6 +14,14 @@
 bool init_led_control();
 
 /*
- * @brief Handles the LED control.
+ * @brief Sets the LED color.
+ * @param red Red component (0-255)
+ * @param green Green component (0-255)
+ * @param blue Blue component (0-255)
+ */
+void set_led_color(uint8_t red, uint8_t green, uint8_t blue);
+
+/*
+ * @brief Handles the LED color cycling.
  */
 void handle_led_control();
